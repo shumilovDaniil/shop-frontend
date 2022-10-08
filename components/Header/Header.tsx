0010dd85e9538e8c77ed84c58c93f0b2e862ef6f
Danil Shumilov <shumilov.dss@gmail.com>
2022-10-08 19:47:20 +0500

    top header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,27 @@
 import React from "react"
+import Link from "next/link"
 
 const Header = () => {
   return (
-    <div>
-      Header start
-    </div>
+    <header>
+      <ul>
+        <li>
+          <Link href="/"><a>Shop</a></Link>
+        </li>
+        <li>
+          <select name="" id="">
+            <option value="Москва">Москва</option>
+            <option value="Санкт-Петербург">Санкт-Петербург</option>
+          </select>
+        </li>
+        <li>
+          <button>Become a Partner</button>
+        </li>
+        <li><a href="tel:78000000000">+7 (800) 322-55-21</a></li>
+        <li><Link href="/call-request"><a href="">Заказать звонок</a></Link></li>
+        <li><Link href="/sign-in">Войти</Link></li>
+      </ul>
+    </header>
   )
 }
 
